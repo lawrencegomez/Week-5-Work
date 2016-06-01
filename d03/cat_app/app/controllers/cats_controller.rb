7de6b58b265de29f0f59862e45f54ef8314513d2
lawrencegomez <lfg2305@gmail.com>
2016-06-01 15:30:16 -0700

    Made a complete CRUD application with Rails, using cats as an example

--- d03/cat_app/app/controllers/cats_controller.rb
+++ d03/cat_app/app/controllers/cats_controller.rb
@@ -20,6 +20,32 @@ class CatsController < ApplicationController
     end
   end
 
+  def edit
+    #for rendering the EDIT CAT page template
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    #SAVE the edited cat to the database
+    @cat = Cat.find(params[:id])
+    #if we can succesfully update the cat attributes, take us to the edit_cat_path view
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_path(@cat)
+    else
+      redirect_to edit_cat_path(@cat)
+    end
+  end
+
+  def destroy
+    #remove the cat from the database
+    @cat = Cat.find(params[:id])
+    if @cat.destroy
+      redirect_to root_path
+    else
+      redirect_to cat_path(@cat)
+    end
+  end
+
 private
   def cat_params
     (params.require(:cat).permit(:name, :age, :gender, :breed, :indoor, :image))
--- d03/cat_app/config/routes.rb
+++ d03/cat_app/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'cats#index'
+
   get '/cats' => 'cats#index'
 
   get '/cats/new' => 'cats#new', as: :new_cat
@@ -8,6 +11,17 @@ Rails.application.routes.draw do
   post '/cats' => 'cats#create'
 
   get '/cats/:id' => 'cats#show', as: :cat
+
+  get '/cats/:id/edit' => 'cats#edit', as: :edit_cat
+
+  # we are using 'patch' because there is already an existing resource in our database that we want
+  # to edit, versus 'put'which doesn't care whether or not there is an existing resource in the database
+  patch '/cats/:id' => 'cats#update'
+
+  delete '/cats/:id' => 'cats#destroy', as: :destroy_cat
+
+  # this is called RESTful routing, based on how it's setup
+
 end
 
 #order of how these are placed matters a lot! REMEMBER THAT! new goes before show
